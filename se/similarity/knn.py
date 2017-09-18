c41193bdf0e0e9d196144220b855474648ba831a
Daniel Chiu <cuc496@psu.edu>
2017-09-18 15:57:27 -0400

    similarity function

--- se/similarity/knn.py
+++ se/similarity/knn.py
@@ -27,16 +27,60 @@ def by_euclidean_distance(id_, k=10):
 
 #TODO
 def by_inner_product(id_, k=10):
-    pass
+    distances = []
+    vector_coll = mongodb_helper.get_coll(settings.VECTOR_COLL)
+    rest = vector_coll.find_one({'id': id_})
+    if rest is None:
+        return []
+
+    v = rest['v']
+    for business in vector_coll.find({'type': settings.BUSINESS_COLL}):
+        v2 = business['v']
+        distance = sum(a*b for a, b in zip(v, v2))
+        distances.append((distance, business['id']))
+    return sorted(distances)[1:k+1]    
 
 #TODO
 def by_sigmoid_inner_product(id_, k=10):
-    pass
+    distances = []
+    vector_coll = mongodb_helper.get_coll(settings.VECTOR_COLL)
+    rest = vector_coll.find_one({'id': id_})
+    if rest is None:
+        return []
 
+    v = rest['v']
+    for business in vector_coll.find({'type': settings.BUSINESS_COLL}):
+        v2 = business['v']
+        distance = 1/(1 + exp(-sum(a*b for a, b in zip(v, v2))))
+        distances.append((distance, business['id']))
+    return sorted(distances)[1:k+1]
+    
 #TODO
 def by_cosine(id_, k=10):
-    pass
+    distances = []
+    vector_coll = mongodb_helper.get_coll(settings.VECTOR_COLL)
+    rest = vector_coll.find_one({'id': id_})
+    if rest is None:
+        return []
+
+    v = rest['v']
+    for business in vector_coll.find({'type': settings.BUSINESS_COLL}):
+        v2 = business['v']
+        distance = sum(a*b for a, b in zip(v, v2))/(sqrt(sum(pow(a,2) for a in v))*sqrt(sum(pow(b,2) for b in v2)))
+        distances.append((distance, business['id']))
+    return sorted(distances)[1:k+1]
 
 #TODO
 def by_manhattan_distance(id_, k=10):
-    pass
+    distances = []
+    vector_coll = mongodb_helper.get_coll(settings.VECTOR_COLL)
+    rest = vector_coll.find_one({'id': id_})
+    if rest is None:
+        return []
+
+    v = rest['v']
+    for business in vector_coll.find({'type': settings.BUSINESS_COLL}):
+        v2 = business['v']
+        distance = sum(abs(a-b) for a, b in zip(v, v2))
+        distances.append((distance, business['id']))
+    return sorted(distances)[1:k+1]
